8649da74f243a383ae2afada3795936b77d85478
Mike <mmusallam@tipbit.com>
2015-02-11 14:33:38 -0500

    Fixed demo code

--- Demo/TestBadge/TestBadge/MasterViewController.m
+++ Demo/TestBadge/TestBadge/MasterViewController.m
@@ -32,18 +32,18 @@
     UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(insertNewObject:)];
     self.navigationItem.rightBarButtonItem = addButton;
     
-    // Build your regular UIBarButtonItem with Custom View
-    UIImage *image = [UIImage imageNamed:@"someImage"];
-    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
-    button.frame = CGRectMake(0,0,image.size.width, image.size.height);
-    [button addTarget:self action:@selector(buttonPress:) forControlEvents:UIControlEventTouchDown];
-    [button setBackgroundImage:image forState:UIControlStateNormal];
-    
-    // Make BarButton Item
-    UIBarButtonItem *navLeftButton = [[UIBarButtonItem alloc] initWithCustomView:button];
-    self.navigationItem.leftBarButtonItem = navLeftButton;
-    self.navigationItem.leftBarButtonItem.badgeValue = @"1";
-    self.navigationItem.leftBarButtonItem.badgeBGColor = self.navigationController.navigationBar.tintColor;
+//    // Build your regular UIBarButtonItem with Custom View
+//    UIImage *image = [UIImage imageNamed:@"someImage"];
+//    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+//    button.frame = CGRectMake(0,0,image.size.width, image.size.height);
+//    [button addTarget:self action:@selector(buttonPress:) forControlEvents:UIControlEventTouchDown];
+//    [button setBackgroundImage:image forState:UIControlStateNormal];
+//    
+//    // Make BarButton Item
+//    UIBarButtonItem *navLeftButton = [[UIBarButtonItem alloc] initWithCustomView:button];
+//    self.navigationItem.leftBarButtonItem = navLeftButton;
+//    self.navigationItem.leftBarButtonItem.badgeValue = @"1";
+//    self.navigationItem.leftBarButtonItem.badgeBGColor = self.navigationController.navigationBar.tintColor;
     
     // new method of adding badges
     UIImage *image2 = [UIImage imageNamed:@"someImage"];
@@ -51,9 +51,9 @@
                                                                       style:UIBarButtonItemStylePlain
                                                                      target:self
                                                                      action:@selector(buttonPress:)];
-    self.navigationItem.rightBarButtonItem = navRightButton;
-    self.navigationItem.rightBarButtonItem.badgeValue = @"2";
-    self.navigationItem.rightBarButtonItem.badgeBGColor = [UIColor orangeColor];
+    self.navigationItem.leftBarButtonItem = navRightButton;
+    self.navigationItem.leftBarButtonItem.badgeValue = @"2";
+    self.navigationItem.leftBarButtonItem.badgeBGColor = [UIColor orangeColor];
     
 }
 
